abeb5eb51d38e6e002d1934e666f7934daa17ea0
= <mananvirmani611@gmail.com>
2021-02-07 22:49:55 +0530

    intial commit

---
+++ PracticeClass.java
@@ -0,0 +1,13 @@
+package practice;
+import java.text.SimpleDateFormat;
+import java.util.*;
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner (System.in);
+	
+	System.out.println("new manan");
+	}
+
+}
---
+++ Secondfile.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Secondfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("manan");
+		System.out.println("virmani");
+	}
+
+}
